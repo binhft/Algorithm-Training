5d204e58cfecd28f13c5d01224752c6094325a46
ittus <vuminhthang.cm@gmail.com>
2017-01-14 14:54:31 +0800

    Maximal Square

---
+++ 221. Maximal Square/src/Solution.java
@@ -0,0 +1,33 @@
+public class Solution {
+	public int maximalSquare(char[][] matrix) {
+		if (matrix.length == 0) {
+			return 0;
+		}
+
+		int m = matrix.length;
+		int n = matrix[0].length;
+
+		int[][] dp = new int[m + 1][n + 1];
+		int result = 0;
+		for (int i = 1; i <= m; i++) {
+			for (int j = 1; j <= n; j++) {
+				if (matrix[i - 1][j - 1] == '1') {
+					dp[i][j] = Math.min(
+							Math.min(dp[i - 1][j - 1], dp[i][j - 1]),
+							dp[i - 1][j]) + 1;
+					result = Math.max(result, dp[i][j]);
+				}
+			}
+		}
+
+		return result * result;
+	}
+
+	public static void main(String[] args) {
+		char[][] input = new char[][] { { '1', '0', '1', '0', '0' },
+				{ '1', '0', '1', '1', '1' }, { '1', '1', '1', '1', '1' },
+				{ '1', '0', '0', '1', '0' } };
+
+		System.out.println(new Solution().maximalSquare(input));
+	}
+}
