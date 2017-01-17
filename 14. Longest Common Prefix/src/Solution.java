13c977e3a75661d6cccb780b411f47310322a377
ittus <vuminhthang.cm@gmail.com>
2017-01-17 18:21:19 +0800

    Longest Common Prefix

---
+++ 14. Longest Common Prefix/src/Solution.java
@@ -0,0 +1,30 @@
+public class Solution {
+	public String longestCommonPrefix(String[] strs) {
+		if (strs.length == 0) {
+			return "";
+		} else if (strs.length == 1) {
+			return strs[0];
+		}
+
+		int minLen = Integer.MAX_VALUE;
+		for (String str : strs) {
+			minLen = Math.min(minLen, str.length());
+		}
+
+		for (int i = 0; i < minLen; i++) {
+			for (int j = 0; j < strs.length - 1; j++) {
+				String s1 = strs[j];
+				String s2 = strs[j + 1];
+				if (s1.charAt(i) != s2.charAt(i)) {
+					return s1.substring(0, i);
+				}
+			}
+		}
+		return strs[0].substring(0, minLen);
+	}
+
+	public static void main(String[] args) {
+		System.out.println(new Solution().longestCommonPrefix(new String[] {
+				"abc", "abcdef", "ab" }));
+	}
+}
