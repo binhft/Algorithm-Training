8012dee6c289680e57727852ab0515a69c58b1d7
ittus <vuminhthang.cm@gmail.com>
2017-06-14 15:06:54 +0700

    Same Tree

---
+++ 100. Same Tree/src/Solution.java
@@ -0,0 +1,14 @@
+public class Solution {
+	public boolean isSameTree(TreeNode p, TreeNode q) {
+		if (p == null && q == null) {
+			return true;
+		} else if (p == null && q != null) {
+			return false;
+		} else if (q == null && p != null) {
+			return false;
+		}
+
+		return p.val == q.val && isSameTree(p.left, q.left)
+				&& isSameTree(p.right, q.right);
+	}
+}
