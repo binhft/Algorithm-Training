d3d0d5349775174e3073158daca071cfbb434cbf
ittus <vuminhthang.cm@gmail.com>
2017-07-18 23:26:36 +0900

    Product of Array Except Self

---
+++ 238. Product of Array Except Self/src/Solution.java
@@ -0,0 +1,20 @@
+public class Solution {
+	public int[] productExceptSelf(int[] nums) {
+		if (nums == null || nums.length == 0) {
+			return null;
+		}
+		int[] res = new int[nums.length];
+		res[0] = 1;
+		for (int i = 1; i < nums.length; i++) {
+			res[i] = res[i - 1] * nums[i - 1];
+		}
+
+		int start = 1;
+		for (int i = nums.length - 1; i >= 0; i--) {
+			res[i] *= start;
+			start *= nums[i];
+		}
+
+		return res;
+	}
+}
