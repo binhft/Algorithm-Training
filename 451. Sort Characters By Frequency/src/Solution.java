39b62aa090d01c568d100ad5f936c7c5743a9f5f
Vu Minh Thang <vuminhthang.cm@gmail.com>
2018-09-28 17:20:57 +0900

    Bucket sort solution

--- 451. Sort Characters By Frequency/src/Solution.java
+++ 451. Sort Characters By Frequency/src/Solution.java
@@ -1,40 +1,65 @@
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 public class Solution {
 	
 	// Hashmap solution
+//	public String frequencySort(String s) {
+//		HashMap<Character, Integer> map = new HashMap<Character, Integer>();
+//		for (int idx = 0; idx < s.length(); idx++) {
+//			char c = s.charAt(idx);
+//			if (map.containsKey(c)) {
+//				map.put(c, map.get(c) + 1);
+//			} else {
+//				map.put(c,  1);
+//			}
+//		}
+//		List<Map.Entry<Character, Integer>> list = new ArrayList<Map.Entry<Character, Integer>>(map.entrySet());
+//		Collections.sort(list, new Comparator<Map.Entry<Character, Integer>> () {
+//            public int compare(Map.Entry<Character, Integer> a, Map.Entry<Character, Integer> b) {
+//            	return b.getValue().compareTo(a.getValue());
+//            }
+//		});
+//		
+//		char[] res = new char[s.length()];
+//		int count = 0;
+//		for (Map.Entry<Character, Integer> item: list) {
+//			for (int idx = 0; idx < item.getValue(); idx++) {
+//				res[count] = item.getKey();
+//				count += 1;
+//			}
+//		}
+//		return String.valueOf(res);
+//	}
+	
+	// bucket solution
 	public String frequencySort(String s) {
 		HashMap<Character, Integer> map = new HashMap<Character, Integer>();
 		for (int idx = 0; idx < s.length(); idx++) {
 			char c = s.charAt(idx);
-			if (map.containsKey(c)) {
-				map.put(c, map.get(c) + 1);
-			} else {
-				map.put(c,  1);
-			}
+			map.put(c, map.getOrDefault(c, 0) + 1);
 		}
-		List<Map.Entry<Character, Integer>> list = new ArrayList<Map.Entry<Character, Integer>>(map.entrySet());
-		Collections.sort(list, new Comparator<Map.Entry<Character, Integer>> () {
-            public int compare(Map.Entry<Character, Integer> a, Map.Entry<Character, Integer> b) {
-            	return b.getValue().compareTo(a.getValue());
-            }
-		});
+		List<Character>[] bucket = new List[s.length() + 1];
+		for (char key: map.keySet()) {
+			int freq = map.get(key);
+			if (bucket[freq] == null) {
+				bucket[freq] = new ArrayList<Character>();
+			}
+			bucket[freq].add(key);
+		}
 		
-		char[] res = new char[s.length()];
-		int count = 0;
-		for (Map.Entry<Character, Integer> item: list) {
-			for (int idx = 0; idx < item.getValue(); idx++) {
-				res[count] = item.getKey();
-				count += 1;
+		StringBuilder sb = new StringBuilder();
+		for (int pos = bucket.length - 1; pos >= 0; pos--) {
+			if (bucket[pos] != null) {
+				for (char c : bucket[pos]) {
+					for (int i = 0; i < map.get(c); i++) {
+						sb.append(c);
+					}
+				}
 			}
 		}
-		return String.valueOf(res);
+		return sb.toString();
 	}
 	
 //	public static void main(String[] args) {
