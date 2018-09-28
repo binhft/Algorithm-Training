72ba29901ae6b1c1247f0637075f354499910b29
Vu Minh Thang <vuminhthang.cm@gmail.com>
2018-09-28 13:00:18 +0900

    Second solution

--- 387. First Unique Character in a String/src/Solution.java
+++ 387. First Unique Character in a String/src/Solution.java
@@ -1,25 +1,42 @@
 class Solution {
+	// First Solution
+//    public int firstUniqChar(String s) {
+//        boolean[] isChecked = new boolean[s.length()];
+//        for (int idx = 0; idx < s.length(); idx++) {
+//        	if (isChecked[idx]) {
+//        		continue;
+//        	}
+//        	boolean isDuplicate = false;
+//        	char c = s.charAt(idx);
+//        	isChecked[idx] = true;
+//        
+//        	for (int sIdx = idx + 1; sIdx < s.length(); sIdx ++) {
+//        		if (isChecked[sIdx]) {
+//            		continue;
+//            	}
+//        		char nextChar = s.charAt(sIdx);
+//        		if (nextChar == c) {
+//        			isChecked[sIdx] = true;
+//        			isDuplicate = true;
+//        		}
+//        	}
+//        	if (!isDuplicate) {
+//        		return idx;
+//        	}
+//        }
+//        return -1;
+//    }
+    
+	// Second Solution
     public int firstUniqChar(String s) {
-        boolean[] isChecked = new boolean[s.length()];
+        int[] freq = new int[26];
         for (int idx = 0; idx < s.length(); idx++) {
-        	if (isChecked[idx]) {
-        		continue;
-        	}
-        	boolean isDuplicate = false;
         	char c = s.charAt(idx);
-        	isChecked[idx] = true;
-        
-        	for (int sIdx = idx + 1; sIdx < s.length(); sIdx ++) {
-        		if (isChecked[sIdx]) {
-            		continue;
-            	}
-        		char nextChar = s.charAt(sIdx);
-        		if (nextChar == c) {
-        			isChecked[sIdx] = true;
-        			isDuplicate = true;
-        		}
-        	}
-        	if (!isDuplicate) {
+        	freq[c - 'a'] += 1;
+        }
+        for (int idx = 0; idx < s.length(); idx++) {
+        	char c = s.charAt(idx);
+        	if (freq[c - 'a'] == 1) {
         		return idx;
         	}
         }
